7b95534f62d019e8facffe25060d51b13ca8f6f5
Chris0024 <chrisfa1998@gmail.com>
2019-10-06 05:21:34 +0200

    cuarto cambio

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ void cambio(int &a , int &b){
 void burbuja(int *arr,int n){
 	int i,j;
 	for(i=0;i<n-1;i++){
-		for(j=0;j<n-i-1;j++){
+		for(j=0;j<n-1-i;j++){
 			if(*(arr+j)>*(arr+j+1))
 				cambio(*(arr+j),*(arr+j+1));
 		}
